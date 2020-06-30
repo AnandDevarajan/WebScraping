3f6a6a301328fa5ba5042a05c15772ea3694850e
Anand Devarajan <ananddevarajan01@gmail.com>
2020-06-30 18:53:35 +0530

    minor bugs fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
-const fs = require(fs);
+const fs = require("fs");
 const json2csv = require("json2csv").Parser;
 
-const phone =
+const phones =
   "https://www.amazon.in/Apple-iPhone-Pro-Max-64GB/dp/B07XVLMZHH/ref=sr_1_2?crid=2XB8OHRIFGY1J&dchild=1&keywords=iphone+11+pro+max+512gb&qid=1593516900&sprefix=iphone+%2Caps%2C364&sr=8-2";
 
 (async () => {
@@ -17,19 +17,16 @@ const phone =
     },
     gzip: true,
   });
-  let $ = cherio.load(response);
-  const title = $(
-    'div[class="a-section a-spacing-none"]>h1>span'
-  ).textContent.trim();
-  const rating = $('div[class="a-row"]>span').textContent;
+  let $ = cheerio.load(response);
+  const title = $('div[class="a-section a-spacing-none"]>h1>span').text();
+  const rating = $('div[class="a-row"]>span').text();
   const review = $(
     'div[class="a-expander-content reviewText review-text-content a-expander-partial-collapse-content"]>span'
-  ).textContent;
-  const size = $('div[class="a-row a-spacing-micro singleton"]>span')
-    .textContent;
+  ).text();
+  const size = $('div[class="a-row a-spacing-micro singleton"]>span').text();
   const price = $(
     'div[class="a-section a-spacing-small"]>table[class="a-lineitem"]>tbody>tr>td[class="a-span12"]>span'
-  ).textContent;
+  ).text();
   phoneData.push({
     title,
     rating,
@@ -37,4 +34,7 @@ const phone =
     size,
     price,
   });
+  const j2cp = new json2csv();
+  const csv = j2cp.parse(phoneData);
+  fs.writeFileSync("./phone.csv", csv, "utf-8");
 })();
